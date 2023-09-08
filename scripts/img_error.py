168c4ae0f8fdf7812d38c672d9563e2b66f68ea0
agustimut98 <agusti.mut1@estudiant.uib.eu>
2023-09-08 11:59:00 +0200

    testbench

--- scripts/img_error.py
+++ scripts/img_error.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import cv2
 import rospy
 import numpy as np
@@ -8,8 +9,26 @@ import numpy as np
 # Inicializar el nodo
 rospy.init_node('img_error', anonymous=True)
 
-RUTA_IMAGEN_A = "/home/agusti/catkin_ws/src/img_compressor_v2/scripts/compressor_original_image/SPIHT/img0.pgm"
-RUTA_IMAGEN_B = "/home/agusti/catkin_ws/src/img_compressor_v2/scripts/joiner_decompressed_image/SPIHT/img0.pgm"
+algritmo = "DEBT"
+img_compressor_type = "/img_compressor_v2/type"
+if rospy.has_param(img_compressor_type):
+    algoritmo = rospy.get_param(img_compressor_type)
+
+if algoritmo == SPIHT:
+    formato_imagen = "pgm"
+    formato formato_compresion = "ims"
+
+elif algoritmo == DEBT:
+    formato_imagen = "pgm"
+    formato formato_compresion = "dbt"
+
+elif algoritmo == JPEG2000:
+    formato_imagen = "jpg"
+    formato formato_compresion = "jp2"
+
+RUTA_IMAGEN_A = "compressor_original_image/{}/img0.{}".format(algoritmo, formato_imagen)
+RUTA_IMAGEN_B = "joiner_decompressed_image/{}/img0.{}".format(algoritmo, formato_imagen)
+RUTA_IMAGEN_C = "compressor_compressed_image/{}/img0.{}".format(algoritmo, formato_compresion)
 
 def mse(imageA, imageB):
     # Calcula el error cuadrático medio entre dos imágenes
@@ -33,3 +52,15 @@ rospy.loginfo("Error Cuadrático Medio (MSE): {:.2f}".format(error))
 # Calcula la Raíz Cuadrada del MSE (RMSE)
 rmse = np.sqrt(error)
 rospy.loginfo("Raíz Cuadrada del Error Cuadrático Medio (RMSE): {:.2f}".format(rmse))
+
+# Calcular tamaño de la imagen comprimida
+compressed_size = os.path.getsize(RUTA_IMAGEN_C)
+units = "B"
+
+if (compressed_size >= 1024):
+    compressed_size /= 1024
+    units = "kB"
+    if (compressed_size >= 1024):
+        compressed_size /= 1024
+        units = "MB"
+rospy.loginfo("El tamaño de la imagen comprimida es de : {} {}".format(compressed_size, units))
